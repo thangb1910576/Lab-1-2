34af87139746e1ecc4488ea2640f8c358df069c5
thangb1910576 <thangb1910576@student.ctu.edu.vn>
2023-02-22 07:17:24 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app/routes/contact_route.js
+++ app/routes/contact_route.js
@@ -6,14 +6,17 @@ const router = express.Router();
 router.route("/")
     .get(contacts.findAll)
     .post(contacts.create)
-    .delete(contacts.deleteAll)
+    .delete(contacts.deleteAll);
+
 
 router.route("/favorite")
-    .get(contacts.findAllFavorite)
+    .get(contacts.findAllFavorite);
+
 
 router.route("/:id")
     .get(contacts.findOne)
     .put(contacts.update)
-    .delete(contacts.delete)
+    .delete(contacts.delete);
 
+    
 module.exports = router;
